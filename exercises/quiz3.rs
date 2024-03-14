0ad7bbbb5e55e98deae2341262c086793e9643bf
user810975 <3224544608@qq.com>
2024-03-14 19:21:58 +0800

    update

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -1,3 +1,4 @@
+
 // quiz3.rs
 //
 // This quiz tests:
@@ -16,60 +17,49 @@
 //
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
+pub struct ReportCard {
+    pub grade: f32,
+    pub student_name: String,
+    pub student_age: u8,
+}
 
-pub enum Grade {  
-    Numeric(f32),  
-    Alphabetic(String),  
-}  
-  
-pub struct ReportCard {  
-    pub grade: Grade,  
-    pub student_name: String,  
-    pub student_age: u8,  
-}  
-  
-impl ReportCard {  
-    pub fn print(&self) -> String {  
-        match &self.grade {  
-            Grade::Numeric(grade) => {  
-                format!("{} ({}) - achieved a grade of {}",  
-                    &self.student_name, &self.student_age, grade)  
-            },  
-            Grade::Alphabetic(grade) => {  
-                format!("{} ({}) - achieved a grade of {}",  
-                    &self.student_name, &self.student_age, grade)  
-            },  
-        }  
-    }  
-}  
-  
-#[cfg(test)]  
-mod tests {  
-    use super::*;  
-  
-    #[test]  
-    fn generate_numeric_report_card() {  
-        let report_card = ReportCard {  
-            grade: Grade::Numeric(2.1),  
-            student_name: "Tom Wriggle".to_string(),  
-            student_age: 12,  
-        };  
-        assert_eq!(  
-            report_card.print(),  
-            "Tom Wriggle (12) - achieved a grade of 2.1"  
-        );  
-    }  
-  
-    #[test]  
-    fn generate_alphabetic_report_card() {  
-        let report_card = ReportCard {  
-            grade: Grade::Alphabetic("A+".to_string()),  
-            student_name: "Gary Plotter".to_string(),  
-            student_age: 11,  
-        };  
-        assert_eq!(  
-            report_card.print(),  
-            "Gary Plotter (11) - achieved a grade of A+"  
-        );  
-    }  
+impl ReportCard {
+    pub fn print(&self) -> String {
+        format!(
+            "{} ({}) - achieved a grade of {}",
+            &self.student_name, &self.student_age, &self.grade
+        )
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn generate_numeric_report_card() {
+        let report_card = ReportCard {
+            grade: 2.1,
+            student_name: "Tom Wriggle".to_string(),
+            student_age: 12,
+        };
+        assert_eq!(
+            report_card.print(),
+            "Tom Wriggle (12) - achieved a grade of 2.1"
+        );
+    }
+
+    #[test]
+    fn generate_alphabetic_report_card() {
+        // TODO: Make sure to change the grade here after you finish the exercise.
+        let report_card = ReportCard {
+            grade: 2.1,
+            student_name: "Gary Plotter".to_string(),
+            student_age: 11,
+        };
+        assert_eq!(
+            report_card.print(),
+            "Gary Plotter (11) - achieved a grade of 2.1"
+        );
+    }
 }
--- exercises/tests/tests6.rs
+++ exercises/tests/tests6.rs
@@ -7,8 +7,6 @@
 // Execute `rustlings hint tests6` or use the `hint` watch subcommand for a
 // hint.
 
-
-
 struct Foo {
     a: u128,
     b: Option<String>,
@@ -17,54 +15,32 @@ struct Foo {
 /// # Safety
 ///
 /// The `ptr` must contain an owned box of `Foo`.
-unsafe fn raw_pointer_to_box(ptr: *mut Foo) -> Box<Foo> {  
-
-    // 使用 ptr::from_raw 从原始指针创建 Box  
-
-    let ret: Box<Foo> = Box::from_raw(ptr);  
-
-    ret  
-
-}  
-
-#[cfg(test)]  
-
-mod tests {  
-
-    use super::*;  
-
-  
-
-    #[test]  
-
-    fn test_success() {  
-
-        // 初始化 Foo 时将 b 字段设置为 Some("hello")  
-
-        let data = Box::new(Foo { a: 1, b: Some("hello".to_owned()) });  
-
-  
-
-        let ptr_1 = &data.a as *const u128 as usize;  
-
-        // 安全性：我们传递一个 Foo 的拥有盒  
-
-        let ret = unsafe { raw_pointer_to_box(Box::into_raw(data)) };  
-
-  
-
-        let ptr_2 = &ret.a as *const u128 as usize;  
-
-  
-
-        // 确保两个指针相同（即它们指向相同的内存地址）  
-
-        assert_eq!(ptr_1, ptr_2);  
-
-        // 确保 ret 的 b 字段是我们设置的值  
-
-        assert_eq!(ret.b, Some("hello".to_owned()));  
-
-    }  
-
+unsafe fn raw_pointer_to_box(ptr: *mut Foo) -> Box<Foo> {
+    // SAFETY: The `ptr` contains an owned box of `Foo` by contract. We
+    // simply reconstruct the box from that pointer.
+    let mut ret: Box<Foo> = unsafe { Box::from_raw(ptr) };
+    // todo!("The rest of the code goes here")
+    ret.b = Some("hello".to_owned());
+    ret
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::time::Instant;
+
+    #[test]
+    fn test_success() {
+        let data = Box::new(Foo { a: 1, b: None });
+
+        let ptr_1 = &data.a as *const u128 as usize;
+        // SAFETY: We pass an owned box of `Foo`.
+        let ret = unsafe { raw_pointer_to_box(Box::into_raw(data)) };
+
+        let ptr_2 = &ret.a as *const u128 as usize;
+
+        assert!(ptr_1 == ptr_2);
+        assert!(ret.b == Some("hello".to_owned()));
+    }
+}
+
